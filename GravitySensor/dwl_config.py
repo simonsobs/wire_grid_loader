d6ecf33dd9458a44c9ec63c717f0baf166426167
Shunsuke <sadachi5@gmail.com>
2020-10-14 20:40:34 +0900

    add GravitySensor

---
+++ GravitySensor/dwl_config.py
@@ -0,0 +1,14 @@
+# Boolean flag for Ethernet to IP
+use_moxa = True
+
+# MOXA IP address
+tcp_ip = '192.168.1.7' # USC540 serial converter
+tcp_port =  26 # CH2
+
+# ttyUSB port
+if not use_moxa:
+    rtu_port = '/dev/ttyUSB2'
+    pass
+
+# Logger directory
+log_dir = 'log';
