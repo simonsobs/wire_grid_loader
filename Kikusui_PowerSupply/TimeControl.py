bef752dd7e390c5cd6195222fe2cf854ccca949f
zhongtianjiaxin <zhongtianjiaxin@gmail.com>
2020-10-27 00:10:39 +0900

    added a little chande to ./src/common.py TimeControl.py powerOn.py

--- Kikusui_PowerSupply/TimeControl.py
+++ Kikusui_PowerSupply/TimeControl.py
@@ -3,7 +3,7 @@
 # Built-in python functions
 import sys
 import time
-import parseCmdLine
+from powerOn import parseCmdLine, powerOn
 
 # Check the python version
 if sys.version_info.major == 2:
@@ -14,18 +14,12 @@ if sys.version_info.major == 2:
 
 ### main function ###
 def TimeControl(voltagelim=0., currentlim=0., timeperiod=0., continuous=False):
-    import powerOn
-
     if continuous==True:
         powerOn(voltagelim, currentlim)
         pass
     else:
         for i in range(1):
-            time1 = time.time()
             powerOn(voltagelim, currentlim, timeperiod, checkvalues=False)
-            time2 = time.time()
-
-            print(time2-time1)
 
 ### main command when this script is directly run ###
 if __name__ == '__main__':
@@ -35,5 +29,5 @@ if __name__ == '__main__':
     currentlim = config.currentlim
     timeperiod = config.timeperiod
 
-    TimeControl(voltagelim, currentlim, timeperiod, checkvalues, continuous)
+    TimeControl(voltagelim, currentlim, timeperiod, continuous)
     pass
