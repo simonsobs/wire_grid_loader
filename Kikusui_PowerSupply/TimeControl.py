c2fe46ab465fffd644db90319dbe834e513e65be
zhongtianjiaxin <zhongtianjiaxin@gmail.com>
2020-11-06 22:50:03 +0900

    modified TimeControl.py for a correct measurement

--- Kikusui_PowerSupply/TimeControl.py
+++ Kikusui_PowerSupply/TimeControl.py
@@ -22,18 +22,21 @@ def TimeControl(voltagelim=0., currentlim=0., timeperiod=0., notmakesure=False):
             print("the rated Voltage of this motor DMN37KA is 12V")
             sys.exit(1)
         if timeperiod > 0.:
-            for i in range(5):
+            for i in range(1):
                 for j in range(6):
                     operation_current = currentlim + i * 0.3
                     operation_time = timeperiod * (j+1)
-                    num_execution = 100
+                    num_execution = 10
                     if operation_current > 3.0:
                         print("operation current is over a range from 0. to 3.0")
                         sys.exit(1)
                     for k in range(int(num_execution)):
                         powerOn(voltagelim, operation_current, operation_time, notmakesure=True)
-                        time.sleep(1.5)
-                pass
+                        time.sleep(3)
+                        pass
+                    time.sleep(5)
+                    pass
+                time.sleep(10)
             pass
         else:
             print("This is a script to measure the relation between operation time of power supply and proceeded angle.")
