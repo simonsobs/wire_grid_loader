878067dc0b80089b976aa5645b9dd349e022922b
Kyoto @ wire grid PC <sadachi5@gmail.com>
2020-12-08 02:04:48 +0900

    fix pmx_config.py dwl_config.py

--- Kikusui_PowerSupply/pmx_config.py
+++ Kikusui_PowerSupply/pmx_config.py
@@ -1,16 +1,16 @@
-import os
+import socket
 
 # Boolean flag for Ethernet to IP
 use_moxa = True
 
 # MOXA IP address
 tcp_port =  23 # CH1
-hostname = os.environ['HOSTNAME'];
+hostname = socket.gethostname();
 print('hostname : {}'.format(hostname));
 if hostname.endswith('hepnet.scphys.kyoto-u.ac.jp'):
   tcp_ip = '192.168.1.70' # USC540 serial converter at Kyoto
 elif hostname == 'cmb-daq01' :
-  tcp_ip = '192.168.0.7' # USC540 serial converter
+  tcp_ip = '192.168.0.7' # USC540 serial converter at Tokyo
 else :
   print('WARNING!! There is no suitable hostname option in "pmx_config.py" to switch IP address for the KIKUSUI power supply.')
   tcp_ip = '10.10.10.0.1'
