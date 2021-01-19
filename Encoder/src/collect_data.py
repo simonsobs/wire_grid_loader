060ad84ce62b83353c0c4d57b237169783352fb9
wire grid PC @ Kyoto (NUC) <NONE>
2021-01-20 08:44:06 +0900

    update

--- Encoder/src/collect_data.py
+++ Encoder/src/collect_data.py
@@ -19,69 +19,74 @@ unsigned_long_int_str = 'L';
 num_overflow_bits = unsigned_long_int_size * 8;  # bits
 REFERENCE_COUNT_MAX=70000; # max num_counts of a grid cycle
 
-# Encoder packet
-'''
-#define ENCODER_COUNTER_SIZE 100
-struct EncoderInfo {
-  unsigned long int header;
-  unsigned long int quad[ENCODER_COUNTER_SIZE];
-  unsigned long int clock[ENCODER_COUNTER_SIZE];
-  unsigned long int clock_overflow[ENCODER_COUNTER_SIZE];
-  unsigned long int refcount[ENCODER_COUNTER_SIZE];
-  unsigned long int error_signal[ENCODER_COUNTER_SIZE];
-};
-'''
-encoder_counter_size = 100;
-encoder_packet_indices = [];
-# encoder header
-encoder_packet_indices.append(0);
-encoder_header = 0x1EAF ;
-encoder_header_size  = unsigned_long_int_size;
-encoder_header_str   = unsigned_long_int_str ;
-# encoder quad
-encoder_packet_indices.append(encoder_packet_indices[-1]+1);
-encoder_quad_size = (encoder_counter_size * unsigned_long_int_size)
-encoder_quad_str  = (encoder_counter_size * unsigned_long_int_str )
-# encoder clock
-encoder_packet_indices.append(encoder_packet_indices[-1]+encoder_counter_size);
-encoder_clock_size   = (encoder_counter_size * unsigned_long_int_size)
-encoder_clock_str    = (encoder_counter_size * unsigned_long_int_str )
-# encoder clock_overflow
-encoder_packet_indices.append(encoder_packet_indices[-1]+encoder_counter_size);
-encoder_clock_overflow_size   = (encoder_counter_size * unsigned_long_int_size)
-encoder_clock_overflow_str    = (encoder_counter_size * unsigned_long_int_str )
-# encoder refcount
-encoder_packet_indices.append(encoder_packet_indices[-1]+encoder_counter_size);
-encoder_refcount_size   = (encoder_counter_size * unsigned_long_int_size)
-encoder_refcount_str    = (encoder_counter_size * unsigned_long_int_str )
-# encoder error_signal
-encoder_packet_indices.append(encoder_packet_indices[-1]+encoder_counter_size);
-encoder_error_signal_size   = (encoder_counter_size * unsigned_long_int_size)
-encoder_error_signal_str    = (encoder_counter_size * unsigned_long_int_str )
-# append last index
-encoder_packet_indices.append(encoder_packet_indices[-1]+encoder_counter_size);
+def
 
-# Encoder packet size
-encoder_packet_size = (
-    encoder_header_size
-    + encoder_quad_size
-    + encoder_clock_size
-    + encoder_clock_overflow_size
-    + encoder_refcount_size
-    + encoder_error_signal_size);
-# String to unpack encoder data
-encoder_unpack_str = ( "%s%s%s%s%s%s%s" % (
-    endian, 
-    encoder_header_str, 
-    encoder_quad_str, 
-    encoder_clock_str, 
-    encoder_clock_overflow_str, 
-    encoder_refcount_str, 
-    encoder_clock_str)
-);
-print('encoder_packet_size= {}'.format(encoder_packet_size));
-print('encoder_unpack_str = {}'.format(encoder_unpack_str));
+def define_encoder_packet(){
+  # Encoder packet
+  '''
+  #define ENCODER_COUNTER_SIZE 100
+  struct EncoderInfo {
+    unsigned long int header;
+    unsigned long int quad[ENCODER_COUNTER_SIZE];
+    unsigned long int clock[ENCODER_COUNTER_SIZE];
+    unsigned long int clock_overflow[ENCODER_COUNTER_SIZE];
+    unsigned long int refcount[ENCODER_COUNTER_SIZE];
+    unsigned long int error_signal[ENCODER_COUNTER_SIZE];
+  };
+  '''
+  encoder_counter_size = 100;
+  encoder_packet_indices = [];
+  # encoder header
+  encoder_packet_indices.append(0);
+  encoder_header = 0x1EAF ;
+  encoder_header_size  = unsigned_long_int_size;
+  encoder_header_str   = unsigned_long_int_str ;
+  # encoder quad
+  encoder_packet_indices.append(encoder_packet_indices[-1]+1);
+  encoder_quad_size = (encoder_counter_size * unsigned_long_int_size)
+  encoder_quad_str  = (encoder_counter_size * unsigned_long_int_str )
+  # encoder clock
+  encoder_packet_indices.append(encoder_packet_indices[-1]+encoder_counter_size);
+  encoder_clock_size   = (encoder_counter_size * unsigned_long_int_size)
+  encoder_clock_str    = (encoder_counter_size * unsigned_long_int_str )
+  # encoder clock_overflow
+  encoder_packet_indices.append(encoder_packet_indices[-1]+encoder_counter_size);
+  encoder_clock_overflow_size   = (encoder_counter_size * unsigned_long_int_size)
+  encoder_clock_overflow_str    = (encoder_counter_size * unsigned_long_int_str )
+  # encoder refcount
+  encoder_packet_indices.append(encoder_packet_indices[-1]+encoder_counter_size);
+  encoder_refcount_size   = (encoder_counter_size * unsigned_long_int_size)
+  encoder_refcount_str    = (encoder_counter_size * unsigned_long_int_str )
+  # encoder error_signal
+  encoder_packet_indices.append(encoder_packet_indices[-1]+encoder_counter_size);
+  encoder_error_signal_size   = (encoder_counter_size * unsigned_long_int_size)
+  encoder_error_signal_str    = (encoder_counter_size * unsigned_long_int_str )
+  # append last index
+  encoder_packet_indices.append(encoder_packet_indices[-1]+encoder_counter_size);
+  
+  # Encoder packet size
+  encoder_packet_size = (
+      encoder_header_size
+      + encoder_quad_size
+      + encoder_clock_size
+      + encoder_clock_overflow_size
+      + encoder_refcount_size
+      + encoder_error_signal_size);
+  # String to unpack encoder data
+  encoder_unpack_str = ( "%s%s%s%s%s%s%s" % (
+      endian, 
+      encoder_header_str, 
+      encoder_quad_str, 
+      encoder_clock_str, 
+      encoder_clock_overflow_str, 
+      encoder_refcount_str, 
+      encoder_clock_str)
+  );
+  print('encoder_packet_size= {}'.format(encoder_packet_size));
+  print('encoder_unpack_str = {}'.format(encoder_unpack_str));
 
+  return encoder_packet
+  
 
 # unpack encoder packet
 def process_encoder_packet(data, parse_index):
@@ -89,11 +94,9 @@ def process_encoder_packet(data, parse_index):
     start_ind = parse_index ;
     end_ind   = start_ind + encoder_packet_size ;
     unpacked_data = np.array(struct.unpack(encoder_unpack_str,data[start_ind:end_ind]));
-    #print('encoder unpack data = {}'.format(unpacked_data));
     # Parse the counter packets
     # Extract the header
     header = unpacked_data[encoder_packet_indices[0]:encoder_packet_indices[1]][0]
-    #print(unpacked_data[encoder_packet_indices[0]:encoder_packet_indices[1]]);
     if header != encoder_header:
         raise RuntimeError("Encoder header error: 0x%04X" % (header))
     # Extract the quadrature data
@@ -108,7 +111,6 @@ def process_encoder_packet(data, parse_index):
     error_data    = unpacked_data[encoder_packet_indices[5]:encoder_packet_indices[6]]
 
     # modify data
-    #print(quad_data);
     quad       = quad_data;
     timercount = clock_data + (overflow_data << num_overflow_bits); # clock_data + overflow_data * 2^(num_overflow_bits)
     position   = (count_data+REFERENCE_COUNT_MAX) % REFERENCE_COUNT_MAX;
@@ -135,7 +137,7 @@ def collect_data(outfilename) :
     return_frames = [];
     # Empty the queue and parse its contents appropriately
     approx_size = collector.queue.qsize()
-    if approx_size>0 : print('approximate size = {}'.format(approx_size));
+    #if approx_size>0 : print('approximate size = {}'.format(approx_size));
   
     for i in range(approx_size):
       # Block=True : Block execution until there is something in the queue to retrieve
@@ -172,7 +174,6 @@ def collect_data(outfilename) :
       # Reset data string
       data = ''
       pass; # end of loop over ``i``
-    #print('end of loop over i');
 
     # write data
     for frame in return_frames :
@@ -183,9 +184,6 @@ def collect_data(outfilename) :
       pass; 
 
     pass; # end of ``while True :``
-  
-  #print('last return frames:');
-  #print('{}'.format(return_frames));
   
   collector.stop();
 
