5ac83a77bde896649663e85f8287782bb5d4bf06
Kyoto @ wire grid PC <sadachi5@gmail.com>
2021-04-05 20:17:54 +0900

    add misc/start_webcamvideo.sh

--- copy_to_beaglebone.sh
+++ copy_to_beaglebone.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -ruvvva --exclude=".*/" ./* beaglebone62:/home/debian/scripts/wire_grid_loader/
-rsync -ruvvva --exclude=".*/" ./* beaglebone72:/home/debian/scripts/wire_grid_loader/
+rsync -ruvva --exclude=".*/" --exclude="*aho*" --exclude="*.out" --exclude="*.dat" --exclude="Kikusui_PowerSupply/log" --exclude="Encoder/src/rawdata" ./* beaglebone1:/home/debian/scripts/wire_grid_loader/
+rsync -ruvva --exclude=".*/" --exclude="*aho*" --exclude="*.out" --exclude="*.dat" --exclude="Kikusui_PowerSupply/log" --exclude="Encoder/src/rawdata" ./* beaglebone2:/home/debian/scripts/wire_grid_loader/
