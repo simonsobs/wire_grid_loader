379a2ecc652eac22badc9321399ec1b1dea70f44
Kyoto @ wire grid PC <sadachi5@gmail.com>
2021-04-05 17:57:24 +0900

    update @ Tokyo

--- Kikusui_PowerSupply/run.sh
+++ Kikusui_PowerSupply/run.sh
@@ -4,7 +4,7 @@ echo "Alarm START!"
 echo $(date)
 
 /home/daq01/Downloads/iwt120ctl/iwt120ctl set ANY 33
-sleep 180
+sleep 120
 
 /home/daq01/Downloads/iwt120ctl/iwt120ctl set ANY 0
 
@@ -13,9 +13,16 @@ echo "Measurement START"
 echo $(date)
 
 (
-    #python3 TimeControl.py -v 12 -c 2.0 -t 0.5 -n
-    #python3 powerOn.py -v 12 -c 2.1 -t 3600
-    echo "Hello"
+    python3 powerOn.py -v 12 -c 3.0 -t 3
+    sleep 120
+    python3 powerOn.py -v 12 -c 1.5 -t 600 -n
+    sleep 60
+    python3 powerOn.py -v 12 -c 2.0 -t 600 -n
+    sleep 60
+    python3 powerOn.py -v 12 -c 2.5 -t 600 -n
+    sleep 60
+    python3 powerOn.py -v 12 -c 3.0 -t 600 -n
+    sleep 60
 ) &
 
 wait $!
