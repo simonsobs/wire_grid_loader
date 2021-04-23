9eb077fc1373afb3945852f59f0f94a06981cfef
Kyoto @ wire grid PC <sadachi5@gmail.com>
2021-04-23 20:37:52 +0900

    update config for limitswitch

--- Actuator/LimitSwitch/limitswitch_config.py
+++ Actuator/LimitSwitch/limitswitch_config.py
@@ -1,13 +1,13 @@
 # GPIO pins
 GPIOpinInfo = [
-        {'name':'LSL1'  ,'pin':'P9_7' , 'label':'Left1Motor'         }, # Left  actuator near the motor
-        {'name':'LSL2'  ,'pin':'P9_8' , 'label':'Left2OppositeMotor' }, # Left  actuator on the opposite side of the motor
-        {'name':'LSL3'  ,'pin':'P9_9' , 'label':'Left3NotConnected'  }, # Left  actuator for spares
-        {'name':'LSL4'  ,'pin':'P9_10', 'label':'Left4NotConnected'  }, # Left  actuator for spares
-        {'name':'LSR1'  ,'pin':'P9_14', 'label':'Right1Motor'        }, # Right actuator near the motor
-        {'name':'LSR2'  ,'pin':'P9_15', 'label':'Right2OppositeMotor'}, # Right actuator on the opposite side of the motor
-        {'name':'LSR3'  ,'pin':'P9_16', 'label':'Right3NotConnected' }, # Right actuator for spares
-        {'name':'LSR4'  ,'pin':'P9_17', 'label':'Right4NotConnected' }, # Right actuator for spares
+        {'name':'LSL1'  ,'pin':'P8_7' , 'label':'Left1Motor'         }, # Left  actuator near the motor
+        {'name':'LSL2'  ,'pin':'P8_8' , 'label':'Left2OppositeMotor' }, # Left  actuator on the opposite side of the motor
+        {'name':'LSL3'  ,'pin':'P8_9' , 'label':'Left3NotConnected'  }, # Left  actuator for spares
+        {'name':'LSL4'  ,'pin':'P8_10', 'label':'Left4NotConnected'  }, # Left  actuator for spares
+        {'name':'LSR1'  ,'pin':'P8_14', 'label':'Right1Motor'        }, # Right actuator near the motor
+        {'name':'LSR2'  ,'pin':'P8_15', 'label':'Right2OppositeMotor'}, # Right actuator on the opposite side of the motor
+        {'name':'LSR3'  ,'pin':'P8_16', 'label':'Right3NotConnected' }, # Right actuator for spares
+        {'name':'LSR4'  ,'pin':'P8_17', 'label':'Right4NotConnected' }, # Right actuator for spares
         #{'name':'test1'  ,'pin':'P9_12', 'label':'GPIO60' },
         #{'name':'test2'  ,'pin':'P9_15', 'label':'GPIO48' },
         #{'name':'test3'  ,'pin':'P9_23', 'label':'GPIO49' },
