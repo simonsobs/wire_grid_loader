5a1a9172603c13369098c0de06acc24407e36bbf
sadachi5 <sadachi5@gmail.com>
2026-05-20 13:26:05 +0000

    update at the site in SATp1

--- Encoder/Beaglebone/run.sh
+++ Encoder/Beaglebone/run.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+# Automatically start in /etc/systemd/system/wiregrid-encoder-pru.service
 
+make clean && make
+
+./Beaglebone_Encoder_DAQ
 #./Beaglebone_Encoder_DAQ test.txt 600
-./Beaglebone_Encoder_DAQ test.txt
+#./Beaglebone_Encoder_DAQ test20230816.txt
 
 #./Beaglebone_Encoder_DAQ Encoder1.bin Encoder2.bin IRIG1.bin IRIG2.bin
